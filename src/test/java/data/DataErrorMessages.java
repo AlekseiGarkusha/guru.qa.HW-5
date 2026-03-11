90888aef7e4b78d18488826f7d60f0d9ce0be5ca
Alex <sapphire6061@gmail.com>
2026-03-11 22:07:26 +0300

    global refactoring

--- src/test/java/data/DataErrorMessages.java
+++
@@ -1,6 +0,0 @@
-package data;
-
-public class DataErrorMessages {
-  public static String notFinedPracticeFormTitle = "❌ Форма 'Practice Form' - не загрузилась";
-  public static String notFinedResultFormTitle = "❌ Форма с результирующими данными пользователя - не загрузилась";
-}
